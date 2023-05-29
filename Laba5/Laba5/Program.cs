6558f648c965902db072850191c1d78f772c54e4
lockard3 <i.ilya2002@mail.ru>
2023-05-29 23:19:27 +0300

    первый коммит в dev ветку

---
+++ Laba5/Laba5/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float num1, num2, result;
+            string operation;
+
+            Console.WriteLine("Введите первое число:");
+            num1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число:");
+            num2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите операцию (+, -, *, /):");
+            operation = Console.ReadLine();
+
+            switch (operation)
+            {
+                case "+":
+                    result = num1 + num2;
+                    Console.WriteLine("Результат: {0}", result);
+                    break;
+                case "-":
+                    result = num1 - num2;
+                    Console.WriteLine("Результат: {0}", result);
+                    break;
+                case "*":
+                    result = num1 * num2;
+                    Console.WriteLine("Результат: {0}", result);
+                    break;
+                case "/":
+                    if (num2 == 0)
+                    {
+                        Console.WriteLine("Ошибка: деление на ноль");
+                    }
+                    else
+                    {
+                        result = num1 / num2;
+                        Console.WriteLine("Результат: {0}", result);
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Ошибка: неверная операция");
+                    break;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
